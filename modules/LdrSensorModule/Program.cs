c68948537c401372e8a019dee4af4ff092ec3dad
Ana Munuera <anabelenmunueragarcia@gmail.com>
2023-11-07 23:41:03 +0100

    first commit

---
+++ modules/LdrSensorModule/Program.cs
@@ -0,0 +1,7 @@
+using LdrSensorModule;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>services.AddHostedService<ModuleBackgroundService>())
+    .Build();
+
+host.Run();
